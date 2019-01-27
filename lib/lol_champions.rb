6ed0afe2f58ded290aa60e243bd4a7ab29440951
Dmitri Samotessov <dimasamo96@gmail.com>
2019-01-27 05:10:56 +0000

    adding some files

--- lib/lol_champions.rb
+++ lib/lol_champions.rb
@@ -1,5 +1,5 @@
-require "lol_champions/version"
-
+require_relative "lol_champions/version"
+require_relative "lol_champions/cli"
 module LolChampions
   class Error < StandardError; end
   # Your code goes here...
---
+++ lib/lol_champions/cli.rb
@@ -0,0 +1,7 @@
+class LolChampions::CLI
+    def call
+        puts "Welcome to League of Legends Chapmion Statistics. Please select and option."
+        puts "1. List all champions"
+        puts "2. List all champions by role"
+    end
+end
