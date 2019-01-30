fa64574662b751c4ebb413e08dfb3fbc47223cd7
Dmitri Samotessov <dimasamo96@gmail.com>
2019-01-30 01:50:46 +0000

    Some CLI class modification

--- lib/lol_champions/cli.rb
+++ lib/lol_champions/cli.rb
@@ -1,13 +1,26 @@
 class LolChampions::CLI
     def call
-        puts "Welcome to League of Legends Chapmion Statistics. Please select and option."
-        puts "1. List all champions"
-        puts "2. List all champions by role"
+        self.welcome
+        self.menu
         newscraper = LolChampions::Scraper.new
         LolChampions::Champion.create_from_collection(newscraper.scrape_champions)
-        LolChampions::Champion.all.each.with_index(1) do |champion, i|
-          champion.display_kda
-          #puts "#{i}.#{champion.role}"
-        end
+        self.list_champions
     end
+
+    def welcome
+      puts "Welcome to League of Legends Chapmion Statistics."
+    end
+
+    def menu
+      puts "1. List all champions"
+      puts "2. List all champions by role"
+      puts "3. Exit"
+    end
+
+    def list_champions
+      LolChampions::Champion.all.each.with_index(1) do |champion, i|
+        puts "#{i}. #{champion.name}"
+      end
+    end
+
 end
