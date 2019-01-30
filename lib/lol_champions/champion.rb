de85e403fe5b46b2a9ede0b9bc5c5dc65f8ef4a8
Dmitri Samotessov <dimasamo96@gmail.com>
2019-01-30 01:36:32 +0000

    Champions can display KDA

--- lib/lol_champions/champion.rb
+++ lib/lol_champions/champion.rb
@@ -1,28 +1,33 @@
 class LolChampions::Champion
-  attr_accessor :name, :popularity, :win_rate, :ban_rate, :kills, :deaths, :assists
-  attr_reader :role
-  @@all = []
+    attr_accessor :name, :popularity, :win_rate, :ban_rate, :kills, :deaths, :assists
+    attr_reader :role
+    @@all = []
 
-  def self.all
-    @@all
-  end
-
-  def initialize(champion_hash)
-    champion_hash.each do |attribute, value|
-      self.send("#{attribute}=", value)
+    def self.all
+      @@all
     end
-    @@all.push(self)
-  end
 
-  def role=(roles)
-    @role = roles.split(",").map do |role|
-      role
+    def initialize(champion_hash)
+      champion_hash.each do |attribute, value|
+        self.send("#{attribute}=", value)
+      end
+      @@all.push(self)
+    end
+
+    def role=(roles)
+      @role = roles.split(",").map do |role|
+        role
+      end
+    end
+
+    def display_kda
+      ratio = (self.kills.to_f + self.assists.to_f)/self.deaths.to_f
+      puts "#{ratio.round(2)} - Average: #{self.kills}/#{self.deaths}/#{self.assists}"
+    end
+
+    def self.create_from_collection(champions_array)
+      champions_array.each do |champion_hash|
+        LolChampions::Champion.new(champion_hash)
+      end
     end
   end
-
-  def self.create_from_collection(champions_array)
-    champions_array.each do |champion_hash|
-      LolChampions::Champion.new(champion_hash)
-    end
-  end
-end
